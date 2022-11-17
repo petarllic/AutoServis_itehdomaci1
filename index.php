26bb5751ea3d9d0db0e681c47005be074bad28cf
Petar Ilic <petar.ilic.00@outlook.com>
2022-11-17 12:53:31 +0100

    Updateovan index.php. Napravljena pocetna strana+Logo.

--- index.php
+++ index.php
@@ -1,12 +1,52 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Auto servisi</title>
+        <meta charset="UTF-8">
+
+    </head>
+
+    <body>
+        <div class="header">
+            <div class="container">
+                <div class="logo"> <a href="index.php"><img src="images/logo.png" alt="Auto servis"></a> </div>
+                <div class="menu"> <a class="toggleMenu" href="#"><img src="images/logo.png" alt="" /> </a>
+                    <ul class="nav" id="nav">
+                        <li><a href="index.php">Početna</a></li>
+                        <li><a href="">Uredjivanje usluga</a></li>
+                    </ul>
+               </div>   
+        
+        </div>
+
+</div>
+
+
+        <div class="about">
+            <div class="container">
+                <section class="title-section">
+                    <h1 class="title-header text-center"> Dostupne usluge auto servisa u gradu </h1>
+                </section>
+                <button class="btn" onclick="sortDesc()">Sortiraj opadajuce</button>
+                <button class="btn" onclick="sortAsc()">Sortiraj rastuce</button>
+            </div>
+        </div><br>
+
+
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <input id="pretraga" type="search" onsearch="search()" class="form-control" placeholder="Pretraga..." onkeyup="search()" size="45">
+                    <div id="usluga">
+                    </div>
+                </div>
+
+                <div class="col-md-6" >
+                    <h3>Spisak svih aktivnih auto servisa u gradu:</h3>
+
+                    <ul class="sidebar" id="auto_servis">
+                    </ul>
+                </div>
+            </div>
+        </div>
